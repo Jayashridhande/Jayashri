712e196de611e4b2a4a586b67da246ed3a99c9d2
Jayashridhande <98275897+Jayashridhande@users.noreply.github.com>
2022-09-26 15:55:32 +0530

    Javascript

---
+++ evenno.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        const number =prompt('enter the number')
+//check the no is even.
+if(number % 2 == 0){
+    console.log( 'number is even');
+}
+//check the no is odd.
+else{
+    console.log('number is odd');
+}
+    </script>
+</body>
+</html>
